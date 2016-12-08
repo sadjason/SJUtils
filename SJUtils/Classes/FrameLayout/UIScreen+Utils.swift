09d2b0c55b99dfdb4139ac34c76ba04ce4c9fb39
张威 <zhangwei72@meituan.com>
2016-12-08 22:27:19 +0800

    update access control

--- SJUtils/Classes/FrameLayout/UIScreen+Utils.swift
+++ SJUtils/Classes/FrameLayout/UIScreen+Utils.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 extension UIScreen {
-    static var width: CGFloat {
+    public static var width: CGFloat {
         return UIScreen.main.bounds.width
     }
     
-    static var height: CGFloat {
+    public static var height: CGFloat {
         return UIScreen.main.bounds.height
     }
 }
